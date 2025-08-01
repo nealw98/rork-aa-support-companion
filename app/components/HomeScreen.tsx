2c94315e51dd1bc234f59b5de137fd0a9a4644ad
nealw98 <nealw98@gmail.com>
2025-08-01 15:43:19 -0600

    Update HomeScreen.tsx

--- app/components/HomeScreen.tsx
+++ app/components/HomeScreen.tsx
@@ -160,7 +160,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 15,
+    marginBottom: 10,
     width: '100%',
   },
   sobrietyCounterContainer: {
